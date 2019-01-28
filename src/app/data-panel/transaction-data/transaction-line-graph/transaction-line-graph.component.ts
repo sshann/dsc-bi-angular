2a9db3c61a52e3933fc0cceb9134e19964477b92
Wesley Shann <sshann95@outlook.com>
2019-01-28 23:00:50 +0100

    Update transactions graph to sum values from the sane day as one unique entry

--- src/app/data-panel/transaction-data/transaction-line-graph/transaction-line-graph.component.ts
+++ src/app/data-panel/transaction-data/transaction-line-graph/transaction-line-graph.component.ts
@@ -37,7 +37,26 @@ export class TransactionLineGraphComponent implements OnInit {
       const index = transaction.type === 'Buy' ? 0 : 1;
       this.data[index].series.push(obj);
     });
+    this.data[0].series = this.sumSameDayTransactions(this.data[0].series);
+    this.data[1].series = this.sumSameDayTransactions(this.data[1].series);
+  }
 
+  private sumSameDayTransactions(array) {
+    const newArray = [];
+    let newArrayIndex = 0;
+
+    newArray.push(array[0]);
+    array.slice(1).forEach((element, index) => {
+      if (newArray[newArrayIndex].name === element.name) {
+        newArray[newArrayIndex].value = (parseFloat(newArray[newArrayIndex].value) + parseFloat(element.value)).toFixed(2);
+      } else {
+        newArrayIndex++;
+        newArray.push(element);
+      }
+    });
+
+
+    return newArray;
   }
 
 }
