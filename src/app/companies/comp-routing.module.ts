3772863518bdd3db3b8802fac32223e64582c1c3
Wesley Shann <sshann95@outlook.com>
2019-02-01 16:30:10 +0100

    Create new company list view and deprecate the old company list view

--- src/app/companies/comp-routing.module.ts
+++ src/app/companies/comp-routing.module.ts
@@ -4,9 +4,11 @@ import {RouterModule, Routes} from '@angular/router';
 import {CompanylistComponent} from './companylist/companylist.component';
 import {AuthGuard} from '../auth/guards/auth-guard.service';
 import {AdminGuard} from '../auth/guards/admin-guard.service';
+import {CompanyComponent} from './company/company.component';
 
 const routes: Routes = [
-  {path: 'companies', component: CompanylistComponent, canActivate: [AuthGuard, AdminGuard]},
+  {path: 'companies-old', component: CompanylistComponent, canActivate: [AuthGuard, AdminGuard]},
+  {path: 'companies', component: CompanyComponent, canActivate: [AuthGuard, AdminGuard]}
 
 ];
 
@@ -15,6 +17,7 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
   ],
+  exports: [RouterModule],
   declarations: []
 })
 export class CompRoutingModule {
--- src/app/companies/companies.module.ts
+++ src/app/companies/companies.module.ts
@@ -1,17 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CompRoutingModule } from './comp-routing.module';
-import { CompanylistComponent } from './companylist/companylist.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CompRoutingModule} from './comp-routing.module';
+import {CompanylistComponent} from './companylist/companylist.component';
 import {HttpClientModule} from '@angular/common/http';
+import {CompanyComponent} from './company/company.component';
+import {CompanyFormDialogComponent} from './company/company-form-dialog/company-form-dialog.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {AngularMaterialModule} from '../angular_material.module';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-	CompRoutingModule,
-	HttpClientModule,
-	
+    CompRoutingModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    AngularMaterialModule,
+    ReactiveFormsModule
   ],
-  declarations: [CompanylistComponent]
+  declarations: [
+    CompanylistComponent,
+    CompanyComponent,
+    CompanyFormDialogComponent
+  ],
+  entryComponents: [
+    CompanyFormDialogComponent
+  ]
 })
-export class CompaniesModule { 
+export class CompaniesModule {
 }
